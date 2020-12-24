adca965f91dc4a59f79be95857b6561791db96ee
Gabe-H <gabe.haarberg@gmail.com>
2020-12-23 17:55:10 -0800

    remove google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -11,23 +11,6 @@ function MyApp({ Component, pageProps }) {
         <title>Knight Light Show</title>
         <link rel="icon" href="/knight.svg" />
       </Head>
-      <head>
-      <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-TFQ2W87T8G"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-TFQ2W87T8G');
-            </script>
-              `,
-          }}
-        />
-      </head>
       <NavBar/>
       <Component {...pageProps} />
     </Provider>
